096fecd9408000358f304c6f17fe3c1d9760bff3
boubad <boubadiarra@gmail.com>
2019-01-16 15:43:01 +0100

    approuter cleanup

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -7,30 +7,6 @@ import Consult from "./features/Consult/redux/Consult";
 //
 const Home = () => <AppState />;
 //
-/*
-const AppRouter = () => (
-    <Router>
-      <div className="infoback">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Accueil</Link>
-            </li>
-            <li>
-              <Link to="/consult/">Consulter</Link>
-            </li>
-            <li>
-              <Link to="/admin/">Administrer</Link>
-            </li>
-          </ul>
-        </nav>
-        <Route path="/" exact={true} component={Home} />
-        <Route path="/admin/" component={Admin} />
-        <Route path="/consult/" component={Consult} />
-      </div>
-    </Router>
-  );
-  */
 const AppRouter = () => (
   <Router>
     <div className="infoback">
